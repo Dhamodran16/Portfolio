2770b7371e288a0862d7874290ea5b3563ffc2a0
Dhamodraprasath CM <149589543+Dhamodran16@users.noreply.github.com>
2025-07-04 05:48:10 +0530

    server.js

--- backend/server.js
+++ backend/server.js
@@ -8,7 +8,7 @@ const app = express();
 const PORT = process.env.PORT || 3003;
 
 const allowedOrigins = [
-  "https://portfolio-client-syuq.onrender.com",
+  "https://portfolioforme.onrender.com",
   "http://localhost:3000"
 ];
 
